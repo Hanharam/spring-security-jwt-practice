10bd59330b5d61821be966ad910d8d9c1fff5618
Hanharam <hanharamm@naver.com>
2026-08-07 23:34:57 +0900

    Docs: .gitignore 추가

---
+++ src/test/java/org/example/seven/SevenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
